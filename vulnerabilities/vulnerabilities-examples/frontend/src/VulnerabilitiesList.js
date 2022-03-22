a220bd41dffd12134e079ec98817a450a363e752
hofls <hofls@ya.ru>
2022-03-22 20:22:28 +0500

    vulnerabilities refactoring

--- vulnerabilities/vulnerabilities-examples/frontend/src/VulnerabilitiesList.js
+++ vulnerabilities/vulnerabilities-examples/frontend/src/VulnerabilitiesList.js
@@ -50,35 +50,7 @@ class VulnerabilitiesList extends Component {
                         <Link to="/insecure-object-reference">Insecure Direct Object References</Link>
                     </li>
                 </ul>
-
-                <ul>
-                    <h4>Others:</h4>
-                    <li>
-                        Insecure Cryptographic Storage - sensitive data should be encrypted (passwords, credit cards..)
-                    </li>
-                    <li>
-                        Insufficient Transport Layer Protection - should use https to avoid man-in-the-middle attack
-                    </li>
-                    <li>
-                        CORS vulnerability - should not rely on CORS to prevent undesired requests, because it is enforced on the client side (browser). <br/>
-                        Can be easily bypassed by alternative clients (curl/wget etc)
-                    </li>
-                    <li>
-                        Remote code execution - should not use user input inside functions that are evaluating code
-                    </li>
-                    <li>
-                        Components with vulnerabilities - should scan app dependencies, to check if there are any known vulnerabilities in them
-                    </li>
-                    <li>
-                        Insufficient Logging & Monitoring - to detect attacks early you should securely log & monitor application
-                    </li>
-                    <li>
-                        Insecure Deserialization - should not trust any data received from the client side (such as item price, discount, etc)
-                    </li>
-                    <li>
-                        Information Leakage - should not leak any info about application (e.g. error stacks)
-                    </li>
-                </ul>
+            
             </nav>
         );
     }
